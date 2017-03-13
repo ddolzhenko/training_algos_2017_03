714224ee62014dc76e6ece6bafc6e9e2548c31ae
Dmitry Dolzhenko <ddolzhenko@gmail.com>
2017-03-13 16:48:45 +0200

    search added without testing

--- intro/intro.cpp
+++ intro/intro.cpp
@@ -2,6 +2,17 @@
 using namespace std;
 
 
+int search(int v[], int size, int key) {
+    for (int i = 0; i < size; ++i) {
+        if (v[i] == key) {
+            return i;
+        }
+    }
+
+    return -1;
+}
+
+
 int main(int argc, char const *argv[])
 {
     cout << "hello" << endl;
