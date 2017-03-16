9fac5f100d6b203b48fd85b138973db039cc0077
Dmitry Dolzhenko <ddolzhenko@gmail.com>
2017-03-16 16:52:07 +0200

    search 2

--- intro/intro.cpp
+++ intro/intro.cpp
@@ -81,7 +81,27 @@ void test_search() {
 
     typedef vector<int> Array;
 
-    test(6, search_2, Array({0,1,2,3,4,5,6,7,8}), 8);
+    auto search = search_2;
+
+    auto key = 8;
+    // key not exists in array
+        test(-1, search, Array(), key); // degerate
+        test(-1, search, Array({key-1}), key); // trivial
+        test(-1, search, Array({key-1, key+1}), key); // trivial2
+        test(-1, search, Array({1,2,3,4,5,7}), key); // general
+        test(-1, search, Array({9,10,11,12}), key); // general
+        test(-1, search, Array({4,1,2,7,10}), key); // general
+    // key exists in array
+        // non appliable // degerate
+        test(0, search, Array({key}), key); // trivial
+        test(0, search, Array({key, key+1}), key); // trivial2
+        test(1, search, Array({key-1, key}), key); // trivial2
+        test(8, search, Array({0,1,2,3,4,5,6,7,key}), key); // general
+        test(0, search, Array({key, 9,10,11,12}), key); // general
+        test(2, search, Array({4,1,key,7,10}), key); // general                
+        
+        test(0, search, Array({key,1,key,7,10}), key); // general                
+        test(2, search, Array({2,1,key,7,key}), key); // general                
 }
 
 
